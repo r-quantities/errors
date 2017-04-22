931ddc5a286f6531b9cb022bf282761a096dd96a
Iñaki Úcar <i.ucar86@gmail.com>
2017-04-22 18:49:24 +0200

    plot.errors docs not needed

--- R/plot.R
+++ R/plot.R
@@ -1,10 +1,3 @@
-#' X-Y plotting with errorbars
-#'
-#' Method for plotting \code{errors} objects.
-#'
-#' @param x object of class errors to plot along the x axis, or, if y is missing, along the y axis.
-#' @param y object of class errors to plot along the y axis, or missing.
-#' @param ... other parameters, passed on to \link{plot.default}.
 #' @export
 plot.errors <- function(x, y, ...) {
   NextMethod()
