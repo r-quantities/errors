e99f9b661753584133ebd756238bf13d359291fb
Iñaki Úcar <i.ucar86@gmail.com>
2017-09-20 19:31:40 +0200

    close #16: correct plot limits

--- R/plot.R
+++ R/plot.R
@@ -1,12 +1,15 @@
 # nocov start
 #' @export
 plot.errors <- function(x, y, ...) {
-  NextMethod()
-
   if (missing(y)) {
     y <- x
     x <- seq_along(x)
   }
+
+  xlim <- c(errors_min(min(x)), errors_max(max(x)))
+  ylim <- c(errors_min(min(y)), errors_max(max(y)))
+
+  NextMethod(xlim=xlim, ylim=ylim)
 
   if (inherits(x, "errors"))
     graphics::segments(errors_min(x), y, errors_max(x), y)
