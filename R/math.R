f587708778871ef03cd2bab477206a67acc0f64d
Iñaki Úcar <i.ucar86@gmail.com>
2017-09-20 18:11:58 +0200

    Math refactorisation

--- R/math.R
+++ R/math.R
@@ -1,44 +1,56 @@
-# abs.errors <- errors are the same
-
 #' @export
-sign.errors <- function(x) {
-  x <- .v(x)
-  NextMethod()
-}
-
-#' @export
-sqrt.errors <- function(x) x^0.5
-
-#' @export
-floor.errors <- function(x, ...) {
-  err <- errors(x)
-  values <- .v(NextMethod())
-  err_round <- abs(.v(x) - values)
-  set_errors(values, err + err_round)
-}
-
-#' @export
-ceiling.errors <- floor.errors
-
-#' @export
-trunc.errors <- floor.errors
-
-#' @export
-round.errors <- floor.errors
-
-#' @export
-signif.errors <- floor.errors
-
-#' @export
-exp.errors <- function(x) {
-  err <- abs(errors(x) * exp(.v(x)))
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-log.errors <- function(x, base = exp(1)) {
-  err <- abs(errors(x) / .v(x) / log(base))
-  structure(NextMethod(), "errors" = err, class = "errors")
+Math.errors <- function(x, ...) {
+  switch(
+    .Generic,
+    "abs" = NextMethod(),
+    "sign" = as.numeric(NextMethod()),
+    "sqrt" = x^0.5,
+    "floor" = , "ceiling" = , "trunc" = , "round" = , "signif" = {
+      err <- errors(x)
+      values <- .v(NextMethod())
+      err_round <- abs(.v(x) - values)
+      set_errors(values, err + err_round)
+    },
+    {
+      e <- switch(
+        .Generic,
+        "exp" = abs(errors(x) * exp(.v(x))),
+        "log" = abs(errors(x) / .v(x) / log(if (missing(...)) exp(1) else c(...)[1])),
+        "expm1" = abs(errors(x) * exp(.v(x))),
+        "log1p" = abs(errors(1+x) / .v(1+x)),
+        "cos" = abs(errors(x) * sin(.v(x))),
+        "sin" = abs(errors(x) * cos(.v(x))),
+        "tan" = errors(x) / cos(.v(x))^2,
+        "cospi" = abs(errors(x) * pi * sin(pi * .v(x))),
+        "sinpi" = abs(errors(x) * pi * cos(pi * .v(x))),
+        "tanpi" = errors(x) * pi / cos(pi * .v(x))^2,
+        "acos" = , "asin" = errors(x) / sqrt(1 - .v(x)^2),
+        "atan" = errors(x) / (1 + .v(x)^2),
+        "cosh" = abs(errors(x) * sinh(.v(x))),
+        "sinh" = abs(errors(x) * cosh(.v(x))),
+        "tanh" = errors(x) / cosh(.v(x))^2,
+        "acosh" = errors(x) / sqrt(.v(x) - 1) / sqrt(.v(x) + 1),
+        "asinh" = errors(x) / sqrt(1 + .v(x)^2),
+        "atanh" = abs(errors(x) / (1 - .v(x)^2)),
+        "cumsum" = propagate(cummatrix(errors(x)))
+      )
+      structure(NextMethod(), "errors" = e, class = "errors")
+    },
+    "cumprod" = {
+      values <- NextMethod()
+      err <- propagate(cummatrix(errors(x)) * t(values / t(cummatrix(.v(x), fill=1))))
+      set_errors(values, err)
+    },
+    "cummax" = , "cummin" = {
+      values <- NextMethod()
+      indexes <- which(values == .v(x))
+      reps <- diff(c(indexes, length(x)+1))
+      err <- rep(errors(x)[indexes], times=reps)
+      set_errors(values, err)
+    },
+    "lgamma" = , "gamma" = , "digamma" = , "trigamma" =
+      stop("method not supported for `errors` objects")
+  )
 }
 
 #' @export
@@ -49,138 +61,9 @@ log10.errors <- function(x) log(x, 10)
 #' @method log2 errors
 log2.errors <- function(x) log(x, 2)
 
-#' @export
-log1p.errors <- function(x) {
-  err <- abs(errors(1+x) / .v(1+x))
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-expm1.errors <- function(x) {
-  err <- abs(errors(x) * exp(.v(x)))
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-sin.errors <- function(x) {
-  err <- abs(errors(x) * cos(.v(x)))
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-cos.errors <- function(x) {
-  err <- abs(errors(x) * sin(.v(x)))
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-tan.errors <- function(x) {
-  err <- errors(x) / cos(.v(x))^2
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-asin.errors <- function(x) {
-  err <- errors(x) / sqrt(1 - .v(x)^2)
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-acos.errors <- asin.errors
-
-#' @export
-atan.errors <- function(x) {
-  err <- errors(x) / (1 + .v(x)^2)
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
 # not a generic!
 # atan2.errors <- function(x, y) {
 #   z <- y/x
 #   err <- errors(z) / (1 + .v(z)^2)
 #   structure(NextMethod(), "errors" = err, class = "errors")
 # }
-
-#' @export
-#' @method sinpi errors
-sinpi.errors <- function(x) {
-  err <- abs(errors(x) * pi * cos(pi * .v(x)))
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-#' @method cospi errors
-cospi.errors <- function(x) {
-  err <- abs(errors(x) * pi * sin(pi * .v(x)))
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-#' @method tanpi errors
-tanpi.errors <- function(x) {
-  err <- errors(x) * pi / cos(pi * .v(x))^2
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-sinh.errors <- function(x) {
-  err <- abs(errors(x) * cosh(.v(x)))
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-cosh.errors <- function(x) {
-  err <- abs(errors(x) * sinh(.v(x)))
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-tanh.errors <- function(x) {
-  err <- errors(x) / cosh(.v(x))^2
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-asinh.errors <- function(x) {
-  err <- errors(x) / sqrt(1 + .v(x)^2)
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-acosh.errors <- function(x) {
-  err <- errors(x) / sqrt(.v(x) - 1) / sqrt(.v(x) + 1)
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-atanh.errors <- function(x) {
-  err <- abs(errors(x) / (1 - .v(x)^2))
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-# TODO: beta/gamma family
-
-#' @export
-cumsum.errors <- function(x) {
-  err <- propagate(cummatrix(errors(x)))
-  structure(NextMethod(), "errors" = err, class = "errors")
-}
-
-#' @export
-cumprod.errors <- function(x) {
-  values <- NextMethod()
-  err <- propagate(cummatrix(errors(x)) * t(values / t(cummatrix(.v(x), fill=1))))
-  set_errors(values, err)
-}
-
-#' @export
-cummax.errors <- function(x) {
-  values <- NextMethod()
-  indexes <- which(values == .v(x))
-  reps <- diff(c(indexes, length(x)+1))
-  err <- rep(errors(x)[indexes], times=reps)
-  set_errors(values, err)
-}
-
-#' @export
-cummin.errors <- cummax.errors
