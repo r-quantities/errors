f20388e5b0522ef29d2966f3ad35fc86ad75fb4a
Lionel Henry <lionel.hry@gmail.com>
2020-06-08 10:10:38 +0200

    Add `expect_errors()` helper

--- tests/testthat/test-tidyverse.R
+++ tests/testthat/test-tidyverse.R
@@ -5,16 +5,14 @@ test_that("errors are proxied and restored", {
   x <- set_errors(1:3, 1:3)
 
   proxy <- vctrs::vec_proxy(x)
-  expect_identical(proxy, data.frame(data = 1:3, errors = 1:3 + 0.0))
+  expect_identical(proxy, data.frame(data = 1:3, errors = as.double(1:3)))
 
   out <- vctrs::vec_restore(proxy, x)
-  expect_equal(out, x)
-
-  # Errors are always promoted by vctrs methods
-  expect_identical(attr(out, "errors"), as.double(1:3))
+  expect_errors(out, 1:3, as.double(1:3))
 })
 
 test_that("can slice errors vectors", {
-  x <- set_errors(1:3, 1:3 + 0.0)
-  expect_equal(vctrs::vec_slice(x, 2:3), x[2:3])
+  x <- set_errors(1:3, 1:3)
+  out <- vctrs::vec_slice(x, 2:3)
+  expect_errors(out, 2:3, as.double(2:3))
 })
