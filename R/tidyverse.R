cd2f51911dbd82bbdd576f773482e6189b7424c7
Lionel Henry <lionel.hry@gmail.com>
2020-06-08 09:50:44 +0200

    Remove old-style coercion methods

--- R/tidyverse.R
+++ R/tidyverse.R
@@ -17,38 +17,13 @@ pillar_shaft.errors <- function(x, ...) {
   pillar::new_pillar_shaft_simple(out, align = "right", min_width = 8)
 }
 
-vec_ptype2.errors <- function(x, y, ...) UseMethod("vec_ptype2.errors")
-vec_ptype2.errors.errors <- function(x, y, ...) x
-vec_ptype2.errors.double <- vec_ptype2.errors.errors
-vec_ptype2.errors.integer <- vec_ptype2.errors.double
-vec_ptype2.double.errors <- function(x, y, ...) y
-vec_ptype2.integer.errors <- vec_ptype2.double.errors
 
-vec_cast.errors <- function(x, to, ...) UseMethod("vec_cast.errors")
-vec_cast.errors.errors <- function(x, to, ...) x
-vec_cast.errors.double <- function(x, to, ...) set_errors(x)
-vec_cast.errors.integer <- vec_cast.errors.double
-vec_cast.double.errors <- function(x, to, ...) as.numeric(x)
-vec_cast.integer.errors <- vec_cast.double.errors
 
 #nocov start
 register_all_s3_methods <- function() {
   register_s3_method("pillar::type_sum", "errors")
   register_s3_method("pillar::pillar_shaft", "errors")
 
-  register_s3_method("vctrs::vec_ptype2", "errors")
-  register_s3_method("errors::vec_ptype2.errors", "errors")
-  register_s3_method("errors::vec_ptype2.errors", "double")
-  register_s3_method("errors::vec_ptype2.errors", "integer")
-  register_s3_method("vctrs::vec_ptype2.double", "errors")
-  register_s3_method("vctrs::vec_ptype2.integer", "errors")
-
-  register_s3_method("vctrs::vec_cast", "errors")
-  register_s3_method("errors::vec_cast.errors", "errors")
-  register_s3_method("errors::vec_cast.errors", "double")
-  register_s3_method("errors::vec_cast.errors", "integer")
-  register_s3_method("vctrs::vec_cast.double", "errors")
-  register_s3_method("vctrs::vec_cast.integer", "errors")
 }
 
 register_s3_method <- function(generic, class, fun=NULL) {
