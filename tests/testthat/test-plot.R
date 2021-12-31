379c6d133c853a5f040d4187c8a713dda25b7b73
Iñaki Úcar <iucar@fedoraproject.org>
2021-12-31 18:52:41 +0100

    improve geom_errors tests

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -26,13 +26,15 @@ test_that("ggplot2 plots work as expected", suppressWarnings({
 
   p0 <- ggplot(iris.e) + aes(Sepal.Length, Sepal.Width, color=Species) +
     geom_point() + theme_bw() + theme(legend.position=c(0.6, 0.8))
-
   p1 <- p0 +
     geom_errorbar(aes(ymin=errors_min(Sepal.Width), ymax=errors_max(Sepal.Width))) +
     geom_errorbarh(aes(xmin=errors_min(Sepal.Length), xmax=errors_max(Sepal.Length)))
-
   p2 <- p0 + geom_errors()
+  p3 <- p0 + geom_errors(aes(x=drop_errors(Sepal.Length)))
+  p4 <- p0 + geom_errors(aes(y=drop_errors(Sepal.Width)))
 
   vdiffr::expect_doppelganger("ggplot2 explicit", p1)
   vdiffr::expect_doppelganger("ggplot2 automatic", p2)
+  vdiffr::expect_doppelganger("ggplot2 y", p3)
+  vdiffr::expect_doppelganger("ggplot2 x", p4)
 }))
