36180df59f857ce1975d5cbfc2e22378ea20a40c
Iñaki Úcar <iucar@fedoraproject.org>
2025-06-23 12:19:56 +0200

    minor adjustments + update NEWS

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -27,10 +27,11 @@ test_that("ggplot2 plots work as expected", suppressWarnings({
   iris.e[1:4] <- lapply(iris.e[1:4], function(x) set_errors(x, x*0.02))
 
   p0 <- ggplot(iris.e) + aes(Sepal.Length, Sepal.Width, color=Species) +
-    geom_point() + theme_bw() + theme(legend.position=c(0.6, 0.8))
+    geom_point() + theme_bw() +
+    theme(legend.position="inside", legend.position.inside=c(0.6, 0.8))
   p1 <- p0 +
     geom_errorbar(aes(ymin=errors_min(Sepal.Width), ymax=errors_max(Sepal.Width))) +
-    geom_errorbarh(aes(xmin=errors_min(Sepal.Length), xmax=errors_max(Sepal.Length)))
+    geom_errorbar(aes(xmin=errors_min(Sepal.Length), xmax=errors_max(Sepal.Length)))
   p2 <- p0 + geom_errors()
   p3 <- p0 + geom_errors(aes(x=drop_errors(Sepal.Length)))
   p4 <- p0 + geom_errors(aes(y=drop_errors(Sepal.Width)))
